67d429fa99fc59df51d4067f7645cfffa27cc558
gil.girones@gmail.com <ubuntu@ip-172-31-18-108.us-west-2.compute.internal>
2013-07-08 15:51:20 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var bufcontent = fs.readFileSync('index.html');
 
+//console.log("The content of index.html " + content);
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  response.send(bufcontent.toString());
 });
 
 var port = process.env.PORT || 5000;
